75fd72eef46c816f96bf23595ad27e70d621cfca
Mohammed Tawileh <mohammedtawileh@gmail.com>
2025-01-04 14:41:31 +0300

    feat: Change the font, add support for code highlighting

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import MarkdownIt from "markdown-it";
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+import sql from "highlight.js/lib/languages/sql";
+import css from "highlight.js/lib/languages/css";
+
+import 'highlight.js/styles/default.css';
+
+hljs.registerLanguage("javascript", javascript);
+hljs.registerLanguage("sql", sql);
+hljs.registerLanguage("css", css);
 
 import "./styles/main.css";
+import "./styles/font.css";
 import "./styles/reset.css";
 
 const state = {
@@ -9,7 +20,16 @@ const state = {
 
 type State = typeof state;
 
-const markdownit = new MarkdownIt();
+const markdownit = new MarkdownIt({
+    highlight: (str, lang) => {
+        if (hljs.listLanguages().includes(lang)) {
+            const validLang = hljs.getLanguage(lang) ? lang : "plaintext";
+            return hljs.highlight(str, { language: validLang }).value;
+        } else {
+            return str;
+        }
+    },
+});
 
 const textarea = document.getElementById(
     "editor"
@@ -20,17 +40,19 @@ if (textarea?.addEventListener("input", render) === null) {
     );
 }
 
-const content = document.querySelector("#printable .content") as (HTMLDivElement | null);
+const content = document.querySelector(
+    "#printable .content"
+) as HTMLDivElement | null;
 const dirbutton = document.getElementById("dir-button");
 
 if (dirbutton?.addEventListener("click", handleDirButton) === null) {
-  console.error(
-      "Error adding an input event listener for the textarea `#editor`"
-  );
+    console.error(
+        "Error adding an input event listener for the textarea `#editor`"
+    );
 }
 
 function handleDirButton() {
-  updateState(state.direction == "rtl" ? "ltr" : "rtl");
+    updateState(state.direction == "rtl" ? "ltr" : "rtl");
 }
 
 function updateState(direction: "ltr" | "rtl"): any {
@@ -40,11 +62,12 @@ function updateState(direction: "ltr" | "rtl"): any {
 
 function updateDom(state: State) {
     if (content) {
-      content.dir = state.direction;
+        content.dir = state.direction;
     }
     if (dirbutton) {
         dirbutton.innerText =
-            "Switch To " + (state.direction == "ltr" ? "rtl" : "ltr").toUpperCase();
+            "Switch To " +
+            (state.direction == "ltr" ? "rtl" : "ltr").toUpperCase();
     }
 }
 
