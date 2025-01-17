d9b03234e001f3e9d4f2cb243222a14ec49a115e
Mohammed Tawileh <mohammedtawileh@gmail.com>
2025-01-17 09:39:28 +0300

    fix: Proper support for antler grammer files `.g4`

--- src/languages/antler.ts
+++ src/languages/antler.ts
@@ -1,51 +1,128 @@
-export default function antler () {
+import { HLJSApi } from "highlight.js";
+
+/**
+ * Highlight.js syntax definition for ANTLR4 grammar files
+ * Language: ANTLR4
+ * Requires: highlight.js v10.0.0 or later
+ */
+export default function antler(hljs: HLJSApi) {
+    const ALPHA = "[A-Za-z]";
+    const ALPHA_NUM = "[A-Za-z0-9_]";
+
     return {
+        name: "ANTLR4",
+        case_insensitive: false,
         keywords: {
             keyword:
-                "grammar | rule | options | tokens | import | fragment | returns | catch | try | mode | lexer | parser",
-            built_in: "true false null",
+                "grammar lexer parser import fragment returns locals throws catch finally mode options tokens channels",
+            built_in: "skip more mode push pop type channel",
         },
+
         contains: [
-            {
-                className: "comment",
-                begin: "//",
-                end: "$",
-            },
-            {
-                className: "comment",
-                begin: "/\\*",
-                end: "\\*/",
-            },
-            {
-                className: "string",
-                begin: '"',
-                end: '"',
-            },
+            // Comments
+            hljs.COMMENT("/\\*", "\\*/", { contains: ["self"] }),
+            hljs.C_LINE_COMMENT_MODE,
+
+            // Strings
             {
                 className: "string",
                 begin: "'",
                 end: "'",
+                contains: [{ begin: "\\\\[\\s\\S]", relevance: 0 }],
             },
+
+            // Rule references
             {
-                className: "number",
-                begin: "\\b\\d+\\b",
+                className: "function",
+                begin: `${ALPHA}${ALPHA_NUM}*\\s*:`,
+                end: ";|=>",
+                returnBegin: true,
+                contains: [
+                    {
+                        className: "title",
+                        begin: `${ALPHA}${ALPHA_NUM}*`,
+                        relevance: 0,
+                    },
+                    {
+                        className: "params",
+                        begin: "\\[",
+                        end: "\\]",
+                        contains: [
+                            hljs.APOS_STRING_MODE,
+                            hljs.QUOTE_STRING_MODE,
+                        ],
+                    },
+                ],
             },
-            {
-                className: "symbol",
-                begin: "\\b(EVENT_ATTRIBUTE|STAR_ATTRIBUTE|EQUALS|ATTRIBUTE_VALUE)\\b",
-            },
-            {
-                className: "operator",
-                begin: "=",
-                end: "=",
-            },
-            {
-                className: "meta",
-                begin: "\\b(rule|grammar|options|tokens|import|fragment|returns|catch|try|mode|lexer|parser)\\b",
-            },
+
+            // Token references
             {
                 className: "variable",
-                begin: "\\b[a-zA-Z_][a-zA-Z0-9_]*\\b",
+                begin: `\\$${ALPHA}${ALPHA_NUM}*`,
+            },
+
+            // Labels
+            {
+                className: "symbol",
+                begin: "#",
+                end: `${ALPHA}${ALPHA_NUM}*`,
+            },
+
+            // Actions
+            {
+                className: "code",
+                begin: "{",
+                end: "}",
+                contains: [
+                    {
+                        begin: "[^{}]",
+                        end: "[^{}]",
+                    },
+                ],
+            },
+
+            // Parser rule references
+            {
+                className: "title",
+                begin: `[A-Z]${ALPHA_NUM}*`,
+            },
+
+            // Lexer rule definitions
+            {
+                className: "string",
+                begin: "\\[",
+                end: "\\]",
+                contains: [hljs.BACKSLASH_ESCAPE],
+            },
+
+            // Operators
+            {
+                className: "operator",
+                begin: "->|\\||\\+|\\*|\\?|\\.\\.|\\.|~|!|&|\\:|\\=|\\+=|\\-=|\\*=",
+            },
+
+            // Arguments
+            {
+                className: "attribute",
+                begin: "\\<",
+                end: "\\>",
+                contains: [
+                    hljs.APOS_STRING_MODE,
+                    hljs.QUOTE_STRING_MODE,
+                    {
+                        className: "number",
+                        begin: hljs.NUMBER_RE,
+                    },
+                ],
+            },
+
+            // Character ranges
+            {
+                className: "regexp",
+                begin: "\\[",
+                end: "\\]",
+                illegal: "\\n",
+                contains: [hljs.BACKSLASH_ESCAPE],
             },
         ],
     };
